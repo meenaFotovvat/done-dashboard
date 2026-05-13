1d3c330a72a5b0a1929605a0304b86d59cce16ba
meenaFotovvat <meenafotovvat@gmail.com>
2026-05-13 13:28:52 +0330

    refactor: clean up Home layout classes and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <Suspense fallback={<div>در حال بارگذاری داشبورد...</div>}>
-        <DashboardContainer />
-      </Suspense>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 px-8 pb-20 font-sans">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        <Suspense fallback={<div>در حال بارگذاری داشبورد...</div>}>
+          <DashboardContainer />
+        </Suspense>
       </main>
     </div>
   );
